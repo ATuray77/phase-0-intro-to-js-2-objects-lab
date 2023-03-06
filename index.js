b7940a0ecd9e73e0ae9383fd35a15a306161fdb3
Aruna Turay <tura_arun@yahoo.com>
2023-03-06 11:42:41 -0500

    objectsLab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+  const employee = {
+    name: "Charles",
+    streetAddress: "6001 Goodfellow dr",
+  };
+
+  function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return { ...obj, [key]: value,};
+  }
+
+ function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+  obj[key] = value;
+  return obj;
+ }
+
+ function deleteFromEmployeeByKey(obj, key) {
+  delete obj.key;
+  return { ...obj[key]};
+ }
+
+ function destructivelyDeleteFromEmployeeByKey(obj, key) {
+  delete obj[key];
+  return obj;
+ }
